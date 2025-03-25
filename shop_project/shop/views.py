be53faa21722a7dc73f2eb7fa708e88638261c4d
VakhtangTsintsadze777 <vaxo19902010@gmail.com>
2025-03-25 21:16:09 +0400

    added htmls and css

--- shop_project/shop/views.py
+++ shop_project/shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from shop.models import Product
 # Create your views here.
+
+def home_view(request):
+    return render(request, "home.html", {"products": Product.objects.all()})
+
+def about_view(request):
+    return render(request, "about.html")
+ 
